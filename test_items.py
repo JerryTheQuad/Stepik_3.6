20f4cc318bfcd0193c2af6c8c68df691f8c30be7
JerryTheQuad <valeranikitenkov@gmail.com>
2023-06-10 13:14:53 +0300

    Added a comment to my code

--- test_items.py
+++ test_items.py
@@ -5,6 +5,7 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_button_add_cart(browser):
     browser.get(link)
+    time.sleep(10) #Чтобы было время проверить язык кнопки:)
     button = browser.find_element(By.CSS_SELECTOR, 'button.btn-add-to-basket')
     assert button, "The Cart Button is not present!"
     
